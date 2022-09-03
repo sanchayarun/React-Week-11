0279bf6812c9d5e0e4dfaf04056e0b9dc53a46a8
sanchayarun <sanchayarun@gmail.com>
2022-09-03 03:07:11 +0100

    state login exercise update

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -5,9 +5,17 @@ const  Login = () => {
     const [username, setUsername] = useState('Enter username');
     const [password, setPassword] = useState('Enter password');
 
+    const handleSubmit = event=> {
+        event.preventDefault();
+        const dataToSend = JSON.stringify({username}) ;
+        const dataToSendTwo = JSON.stringify({password});
+        
+        console.log(dataToSend + "  "+ dataToSendTwo);
+    }
+
 return(
 
-    <form>
+    <form onSubmit={event =>handleSubmit(event)}>
         <label htmlFor = "username"> Username: &nbsp;</label>
         <input type = "text" name = "username" value = {username} onChange = {event => setUsername(event.target.value)} />
         <label htmlFor = "password"> Password: &nbsp;</label>
